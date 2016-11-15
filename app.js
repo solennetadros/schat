8e173523ebcfb1e8b7010cd4313ed597128c710b
Solenne Tadros <solennetadros@Solennes-MacBook-Pro.local>
2016-11-15 11:25:51 -0500

    chat chat chat

--- app.js
+++ app.js
@@ -1,52 +1,46 @@
-
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
-var server = require('http').Server(app);
-var port = 9000;
-var io = require('socket.io')(server);
 
-// function mainReqHandler(req, res){
-// 	//res.send("Hello World");
-// 	res.sendFile(__dirname + '/public/index.html');
-// }
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-//app.get('/', mainReqHandler);
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', index);
+app.use('/users', users);
 
-function serverUpCallback(){
-	console.log("listening on port: " + port);
-}
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-function incomingSocketHandler(socket){
-	console.log('a user has connected');
-	// console.log(socket);
-	// console.log(socket.handshake.headers['user-agent']);
-	// console.log(socket.conn.server.clientsCount);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// socket.userName = "User " + socket.conn.server.clientsCount;
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-	socket.emit("welcome message", "Welcome " );
-	socket.on('chat message', function(dataFromClient){
-		socket.userName = dataFromClient.userName;
-		console.log(dataFromClient);
-		var dataFromServer = {
-			userName : socket.userName,
-			message : dataFromClient.msgText
-		}
-		console.log(dataFromServer);
-		io.emit('latest message', dataFromServer);
-	});
-
-	socket.on('new user', function(userName){
-		io.emit("new user server response", userName );
-	});
-
-	socket.on('disconnect', function(){
-		console.log("User has disconnected");
-	});
-}
-
-io.on('connection', incomingSocketHandler);
-
-server.listen(port, serverUpCallback);
+module.exports = app;
